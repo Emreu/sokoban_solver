8661587c5fec486bcbadeadd1caf70f6b222c46f
Anton Kryukov <kryukov@selectel.ru>
2020-10-05 12:43:01 +0300

    Add viewer GUI stub

--- visualizer/Preview.gd
+++ visualizer/Preview.gd
@@ -11,9 +11,9 @@ var tile_size = 8
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tiles = Image.new()
-	print(tiles.load("assets/preview.svg"))
+	if tiles.load("assets/preview.svg") != OK:
+		print("Can't load preview tiles image")
 	tiles.convert(Image.FORMAT_RGB8)
-	print(tiles.data.format)
 	var tex = makeTexture(128,128)
 	$TextureRect.texture = tex
 	
@@ -26,16 +26,13 @@ func makeTexture(w, h):
 			var i = randi() % 8
 			img.blit_rect(tiles, Rect2(i*tile_size, 0, 8, 8), Vector2(x*tile_size, y*tile_size))
 	var tex = ImageTexture.new()
-	print(tex.create_from_image(img))
+	tex.create_from_image(img, 0)
 	return tex
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
-func _process(delta):
-	var tex = makeTexture(128,128)
-	$TextureRect.texture = tex
+#func _process(delta):
+#	var tex = makeTexture(128,128)
+#	$TextureRect.texture = tex
 
 func _on_Button_pressed():
 	var tex = makeTexture(128,128)
