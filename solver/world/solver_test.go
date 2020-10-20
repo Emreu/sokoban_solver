ba3ef517a96535adca394b4bf17db15578bf6d0f
Anton Kryukov <kryukov@selectel.ru>
2020-10-20 21:38:44 +0300

    Add solver test

---
+++ solver/world/solver_test.go
@@ -0,0 +1,38 @@
+package world
+
+import (
+	"context"
+	"strings"
+	"testing"
+)
+
+var solverLevel = `
+    #####
+    #   #
+    #$  #
+  ###  $##
+  #  $ $ #
+### # ## #   ######
+#   # ## #####  ..#
+# $  $          ..#
+##### ### #@##  ..#
+    #     #########
+    #######
+`
+
+func TestSolver(t *testing.T) {
+	r := strings.NewReader(solverLevel)
+	m, err := ReadMap(r)
+	if err != nil {
+		t.Fatalf("Level reading error: %v", err)
+	}
+
+	solver := NewSolver(m)
+
+	ctx := context.Background()
+
+	err = solver.Solve(ctx, false)
+	if err != nil {
+		t.Fatalf("Solving error: %v", err)
+	}
+}
